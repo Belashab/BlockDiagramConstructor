8ea66dc1a93a50c7007aa3cd69e7e60c18f7cee6
Belashab <belashabb@gmail.com>
2021-05-31 16:17:47 +0300

    reserve

--- src/Components/App components/Component_menu.js
+++ src/Components/App components/Component_menu.js
@@ -1,16 +1,20 @@
-import React, { Component } from 'react'
-// import blocks from './blocks/blocks.json'
+import React, { Component } from 'react';
+import blocks from './../Blocks/blocks.json'
 
 function Component_menu(props) {
     return (
         <div>
-            <div className="component_menu__state_arrow"></div>
+            <div className="component_menu__state_arrow" onClick = {component_menu__arrow_click_handler}></div>
             <Component_block id = {1}/>
             <Component_block />
             <Component_block />
             <Component_block />
         </div>
     );
+}
+
+function component_menu__arrow_click_handler (e) {
+    
 }
 
 function component_block__dnd_handler (e) {
