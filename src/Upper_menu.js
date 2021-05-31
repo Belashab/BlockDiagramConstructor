cc39068e752356503abe49b5db2a265a32e10b51
Belashab <belashabb@gmail.com>
2021-05-31 12:26:23 +0300

    reserve commit

--- src/Upper_menu.js
+++ src/Upper_menu.js
@@ -1,14 +1,26 @@
 import React from 'react'
-import Menu, { SubMenu, MenuItem } from 'rc-menu';
 
 function Upper_menu (props) {
     return (
-        <Menu>
-            <MenuItem>1</MenuItem>
-            <SubMenu title="2">
-                <MenuItem>2-1</MenuItem>
-            </SubMenu>
-        </Menu>
+       <div> 
+        <Menu_button id = {1} name = "1" OnClick={menu_button__click_handler}/>
+        <Menu_button id = {2} name = "2" OnClick={menu_button__click_handler}/>
+        <Menu_button id = {3} name = "3" OnClick={menu_button__click_handler}/>
+        <Menu_button name = 'test'/>
+       </div>
+    );
+}
+
+function menu_button__click_handler () {
+    return (
+        console.log('клик')
+    );
+}
+
+
+const Menu_button = (props) => {
+    return (
+        <button id = {props.id} OnClick = {()=> console.log('click')}>{props.name}</button>
     );
 }
 
